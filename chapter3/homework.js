b0588bde6f999e7e91e1fb2bc31b0aca525c7185
sdw1211 <sdw1211@gmail.com>
2018-10-29 16:38:43 +0900

    소스 리팩토링

--- chapter3/homework.js
+++ chapter3/homework.js
@@ -1,60 +1,70 @@
 const app = require('express')();
-const analyzeError = error => console.error(error);
-const errorlog = error => console.error(error);
 
-const every = (req, res, next) => {
-    console.log('every');
-    next();
+const logger = (msg, type='log') => {
+    console[type] && console[type].call(null, msg);
 };
 
-const everyPost = (req, res, next) => {
-    if (req.method === 'POST') {
-        console.log('every post');
+const errorLogger = msg => logger(msg, 'error');
+
+const analyzeError = error => errorLogger(error);
+const errorlog = error => errorLogger(error);
+
+const middlewareTemplate = (func = () => {}, filter = () => true) => (req, res, next) => {
+    if (filter(req, res)) {
+        func();
     }
-    
+
     next();
 };
+
+const every = middlewareTemplate(() => logger('every'));
+const everyPost = middlewareTemplate(() => logger('every post'), (req, res) => req.method === 'POST');
 
 app.use(every, everyPost);
 
 app.get('/users', (req, res) => {
     const users = {users: ['sdw1211', 'killerdong']};
-    console.log(`users list: ${users.toString()}`);
+    logger(`users list: ${users.toString()}`);
     res.json(users);
 });
 
 app.post('/users', (req, res) => {
-    console.log(`get users: ${{users: ['sdw1211', 'killerdong']}.toString()}`);
-    const random = Math.floor(Math.random() * 10);
 
-    if (random === 7) {
-        throw new Error('Random Error');
-    }  
+    const tenPercentFailureMaker = ({success = () => {}, failure = () => {}}) => {
+        const random = Math.floor(Math.random() * 10);
+        const EXCEPTION_NUMBER = 7;
 
-    res.json({msg: '입력완료'});
+        random === EXCEPTION_NUMBER ? failure() : success();
+    };
+
+    tenPercentFailureMaker({
+        success: () => res.json({msg: '입력 성공'})
+        , failure: () => {throw new Error('입력 실패')}});
 });
 
 app.use('/error', (req, res) => {
     throw new Error('Error Test');
 });
 
-app.use((req, res) => {
+const noHandler = (req, res) => {
     res.status(404);
     res.send('404 ERROR');
-});
+};
+
+app.use(noHandler);
 
 const errorHandler1 = (err, req, res, next) => {
     analyzeError(err);
-    next(error);
+    next(err);
 };
 
 const errorHandler2 = (err, req, res, next) => {
     errorlog(err);
-    res.json({msg: '에러!!!'});
+    res.json({msg: err.message});
 };
 
 app.use(errorHandler1, errorHandler2);
 
-app.listen(3000, () => console.log(`Express started on http://localhost:3000; press Ctrl + c to terminate.`));
+app.listen(3000, () => logger(`Express started on http://localhost:3000; press Ctrl + c to terminate.`));
 
 
